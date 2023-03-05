5eb1510bc23c56d4e4c4b9620851f604987b80fb
Dmitriy Plaschinskiy <scancity@yandex.ru>
2023-03-05 17:01:59 +0300

    change script

--- 1.2/script.js
+++ 1.2/script.js
@@ -1,17 +1,12 @@
 // Создание таблицы произвольного размера и заполнение данными вручную по кнопке
 // Создание массива с 30-ю случайными числами от 0 до 99
-let numarray = [];
 let rows = 5;
 let columms = 6;
-let sizearray = rows * columms;
-for (let n = 0; n < sizearray; n++) {
-    numarray.push(Math.floor(Math.random() * 100));
-}
 
 // Поиск div c id=#tablefornum в html-файле
 let numtable = document.querySelector('#tablefornum');
 
-// Функция создания таблицы и заполнение ее значениями из массива numarray
+// Функция создания пустой таблицы
 function createTable(root, columns, rows) {
     let table = document.createElement('table');
     for (let r = 0; r < rows; r++) {
@@ -26,17 +21,17 @@ function createTable(root, columns, rows) {
 }
 
 let count = 0;
-
+// Функция заполнения пустой таблицы случайными значениями вручную по кнопке
 function numberadd() {
     let number=0;
     let table = document.querySelector('table');
-    for (let row of table.rows) {
-        for (let cell of row.cells) {
+    for (let row of table.rows) { // переберем все ряды таблицы с помощью свойства rows;
+        for (let cell of row.cells) { // а в каждом ряду переберем его ячейки с помощью свойства cells;
             if (number==count){
             cell.innerHTML = Math.floor(Math.random() * 100);
-            if (cell.innerHTML >= 50) { // красим ячейку в оранжевый, если число в массиве больше или равно 50;
-				cell.style.backgroundColor = 'orange';
-			  }
+                if (cell.innerHTML >= 50) { // красим ячейку в оранжевый, если число в массиве больше или равно 50;
+                    cell.style.backgroundColor = 'orange';
+                    }
             count+=1;
             return 0;
             }
